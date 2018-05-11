34e5707fb1d0d3cbd2352d015fa88a23fffe73ad
Jesse Wei <jesse.wei@activenetwork.com>
2018-05-11 11:08:50 +0800

    Updated systeminfo datafix template

--- Common/Templates/ANE-76662_systeminfo.sql
+++ Common/Templates/ANE-76662_systeminfo.sql
@@ -1,10 +1,10 @@
-USE cranberrytownship
+USE vancouver
 
 SET XACT_ABORT ON
 
-DECLARE @subject varchar(max)		= 'ANE-72921 P2 - ESI - receipts not printing when membership is scanned'
+DECLARE @subject varchar(max)		= 'ANE-76662 P2 - ESI- Pending Enrollment was not cleared - activity #156781'
 DECLARE @author varchar(max)		= 'Jesse Wei'
-DECLARE @created_date datetime		= '2/5/2018'
+DECLARE @created_date datetime		= '5/11/2018'
 DECLARE @type varchar(max)			= 'Datafix'
 DECLARE @description varchar(max)	= ''
 DECLARE @org varchar(100) = DB_NAME()
@@ -12,19 +12,26 @@ DECLARE @return int
 EXEC @return = rollbackdb.dbo.audit_datafix @org, @subject, @author, @created_date, @type, @description
 IF @@ERROR > 0 OR @return = -1 RETURN
 
-DECLARE @keyword varchar(50) = 'enable_java_applet'
+DECLARE @keyword varchar(50) = 'enable_init_integrity_checks'
 DECLARE @prev_value varchar(50) = 'false'
 DECLARE @new_value varchar(50) = 'true'
 
-IF NOT EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @keyword AND KEYWORDVALUE LIKE @prev_value) BEGIN
-	PRINT 'Nothing to fix.'
+IF EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @keyword AND KEYWORDVALUE LIKE @new_value) BEGIN
+	PRINT 'Nothing to fix. The keyword already has expected value.'
 	RETURN
 END
 
 BEGIN TRANSACTION
 
-	UPDATE SYSTEMINFO SET KEYWORDVALUE = @new_value WHERE KEYWORD = @KEYWORD AND KEYWORDVALUE LIKE @prev_value
-	INSERT INTO SYSTEMINFOLOG (KEYWORD, PREV_KEYWORD_VALUE, SYSTEMUSER_ID, DATESTAMP) VALUES (@KEYWORD, @prev_value, 0, GETDATE())
+	IF EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @KEYWORD) BEGIN
+		UPDATE SYSTEMINFO SET KEYWORDVALUE = @new_value WHERE KEYWORD = @KEYWORD AND KEYWORDVALUE LIKE @prev_value
+	END
+	ELSE BEGIN
+		INSERT INTO SYSTEMINFO (KEYWORD, KEYWORDVALUE) VALUES (@KEYWORD, @new_value)
+	END
+	IF @@ROWCOUNT > 0 BEGIN
+		INSERT INTO SYSTEMINFOLOG (KEYWORD, PREV_KEYWORD_VALUE, SYSTEMUSER_ID, DATESTAMP) VALUES (@KEYWORD, @prev_value, 0, GETDATE())
+	END
 	
 	PRINT char(10) + 'Done. Datafix deployed.'
 COMMIT TRANSACTION
--- Common/Templates/ANE-76662_systeminfo_rollback.sql
+++ Common/Templates/ANE-76662_systeminfo_rollback.sql
@@ -1,10 +1,10 @@
-USE cranberrytownship
+USE vancouver
 
 SET XACT_ABORT ON
 
-DECLARE @subject varchar(max)		= 'ANE-72921 P2 - ESI - receipts not printing when membership is scanned'
+DECLARE @subject varchar(max)		= 'ANE-76662 P2 - ESI- Pending Enrollment was not cleared - activity #156781'
 DECLARE @author varchar(max)		= 'Jesse Wei'
-DECLARE @created_date datetime		= '2/5/2018'
+DECLARE @created_date datetime		= '5/11/2018'
 DECLARE @type varchar(max)			= 'Rollback'
 DECLARE @description varchar(max)	= ''
 DECLARE @org varchar(100) = DB_NAME()
@@ -12,24 +12,30 @@ DECLARE @return int
 EXEC @return = rollbackdb.dbo.audit_datafix @org, @subject, @author, @created_date, @type, @description
 IF @@ERROR > 0 OR @return = -1 RETURN
 
-DECLARE @keyword varchar(50) = 'enable_java_applet'
+DECLARE @keyword varchar(50) = 'enable_init_integrity_checks'
 DECLARE @prev_value varchar(50) = 'false'
 DECLARE @new_value varchar(50) = 'true'
 
-IF EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @keyword AND KEYWORDVALUE LIKE @prev_value) BEGIN
+--No record
+IF NOT EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @keyword) BEGIN
 	PRINT 'Nothing to roll back. Datafix has not been deployed yet.'
 	RETURN
 END
-ELSE IF (SELECT TOP 1 SYSTEMUSER_ID FROM SYSTEMINFOLOG WHERE KEYWORD = @keyword AND PREV_KEYWORD_VALUE LIKE @prev_value ORDER BY SYSTEM_INFO_LOG_ID DESC) = 0 BEGIN
-	PRINT 'Have found the fix. Rolling back...'
+--Not fixed
+IF NOT EXISTS(SELECT * FROM SYSTEMINFO WHERE KEYWORD = @keyword AND KEYWORDVALUE LIKE @new_value) BEGIN
+	PRINT 'Nothing to roll back. Current value is not the new value.'
+	RETURN
 END
 ELSE BEGIN
-	PRINT 'Nothing to roll back. Datafix has not been deployed yet or already been rolled back.'
-	RETURN
+	DECLARE @last_log_id int = (SELECT MAX(SYSTEM_INFO_LOG_ID) FROM SYSTEMINFOLOG WHERE KEYWORD = @keyword)
+	--The last change is NOT made by our data fix
+	IF NOT EXISTS (SELECT * FROM SYSTEMINFOLOG WHERE SYSTEM_INFO_LOG_ID = @last_log_id AND PREV_KEYWORD_VALUE LIKE @prev_value AND SYSTEMUSER_ID = 0) BEGIN
+		PRINT 'Nothing to roll back. Haven''t found the fix.'
+		RETURN
+	END
 END
 
 BEGIN TRANSACTION
-
 	UPDATE SYSTEMINFO SET KEYWORDVALUE = @prev_value WHERE KEYWORD = @KEYWORD AND KEYWORDVALUE LIKE @new_value
 	INSERT INTO SYSTEMINFOLOG (KEYWORD, PREV_KEYWORD_VALUE, SYSTEMUSER_ID, DATESTAMP) VALUES (@KEYWORD, @new_value, 0, GETDATE())
 	
